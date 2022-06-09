2817260355cf0e2c5ae2fed02c2d1c2c2f3934fb
Sam Darmali <samdarmali@Sams-MacBook-Pro-2.local>
2022-06-09 13:54:00 +0800

    v1.01.03 - connect contract functionality.

--- ExternalJS-1.js
+++ ExternalJS-1.js
@@ -1,6 +1,6 @@
 const { ethers } = require("ethers");
 
-console.log("You ran an external JS file v1.01.02");
+console.log("You ran an external JS file v1.01.03");
 
 /* Functions Start */
 const checkNetwork = async () => {
@@ -36,7 +36,8 @@ const checkConnection = async () => {
                 account.length
             )} Wallet Connected 
             `;
-            document.getElementById("connect-wallet-button").innerHTML = element;
+            document.getElementById("connect-wallet-button").innerHTML =
+                element;
         } else {
             console.log("No authorized account found");
             window["account"] = null;
@@ -62,7 +63,7 @@ const connectWallet = async () => {
             method: "eth_requestAccounts",
         });
         console.log("Connected", accounts[0]);
-        window["currentAccount"] = (accounts[0]);
+        window["currentAccount"] = accounts[0];
     } catch (error) {
         console.log(error);
     }
@@ -77,7 +78,7 @@ const attachConnectWalletButton = () => {
 const connectContract = async () => {
     const { ethereum } = window;
     if (ethereum) {
-        if (state["account"] !== null) {
+        if (window["account"]) {
             const provider = new ethers.providers.Web3Provider(ethereum);
             const signer = provider.getSigner();
             const mintingContract = new ethers.Contract(ADDRESS, abi, signer);
@@ -86,28 +87,75 @@ const connectContract = async () => {
 
             // insert into DOM
             const element = `
-                ${state["contract"].address.substring(0, 5)}...${state[
+                ${window["contract"].address.substring(0, 5)}...${window[
                 "contract"
             ].address.substring(
-                state["contract"].address.length - 5,
-                state["contract"].address.length
+                window["contract"].address.length - 5,
+                window["contract"].address.length
             )} Contract Connected 
             `;
-            document.getElementById("connected-contract-div").innerHTML =
+            document.getElementById("connected-contract-p").innerHTML =
                 element;
         } else {
             const element = `
                 No Contract Connected 
             `;
-            document.getElementById("connected-contract-div").innerHTML =
+            document.getElementById("connected-contract-p").innerHTML =
                 element;
         }
     } else {
         console.log("Ethereum object not found");
     }
 };
-/* Functions End */
 
+const getTotalMinted = async () => {
+    const _totalMinted = await window["contract"].getCurrentSupply();
+    window["totalMinted"] = _totalMinted.toNumber();
+};
+
+const getTotalSupply = async () => {
+    const _totalSupply = await window["contract"].maxSupply();
+    window["totalSupply"] = _totalSupply.toNumber();
+};
+
+const getCost = async () => {
+    const _cost = await window["contract"].cost();
+    window["cost"] = ethers.utils.formatEther(_cost);
+};
+
+const updateAmt = (event) => {
+    window["amount"] = parseInt(event.target.value);
+};
+
+const attachInputListener = () => {
+    document
+        .getElementById("mint-amount-input")
+        .addEventListener("change", updateAmt);
+};
+
+const mint = async () => {
+    try {
+        if (window["contract"]) {
+            console.log(`Minting ${window["amount"]}`);
+            const mintTxn = await window["contract"].mint(window["amount"], {
+                value: ethers.utils.parseEther(
+                    (window["amount"] * parseFloat(window["cost"])).toString()
+                ),
+                gasLimit: state["amount"] * 200000,
+            });
+            await mintTxn.wait();
+            console.log("Mint transaction: ", mintTxn);
+            await getTotalMinted();
+        }
+    } catch (error) {
+        console.log("Minting transaction erorr", error);
+    }
+};
+
+const attachMintListener = () => {
+    document.getElementById("mint-button").addEventListener("click", mint);
+};
+/* Functions End */
 
 /* Event Listeners Start */
 window.addEventListener("load", async () => {
@@ -128,11 +176,23 @@ window.addEventListener("load", async () => {
     await connectContract();
 
     // attach function to connect wallet button
-    if (!window.account) {
-        console.log("Attaching connect wallet function...")
+    if (!window["account"]) {
+        console.log("Attaching connect wallet function...");
         attachConnectWalletButton();
     }
 
+    // attach contract details to mint section
+    // if (window["contract"]) {
+    //     await getTotalMinted();
+    //     await getTotalSupply();
+    //     await getCost();
+
+    //     // add event listener to the input section
+    //     attachInputListener();
+
+    //     // add event listener to mint button
+    //     attachMintListener();
+    // }
 });
 
 /* Event Listeners End */
